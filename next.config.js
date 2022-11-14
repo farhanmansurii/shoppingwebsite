abbcf0ff10125b4e421c0671de07006aedc579eb
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-11-14 20:38:59 +0530

    hu

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+
   reactStrictMode: true,
 }
-
+module.exports = {
+  images: {
+    domains: ["img4.gadgetsnow.com"],
+  },
+}
 module.exports = nextConfig
